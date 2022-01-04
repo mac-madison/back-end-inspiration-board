9938be208ce378056c47e6a43103630575dbd6c2
Ana Gabriele <gabbanasoares@gmail.com>
2022-01-04 10:23:48 -0800

    Merge branch 'as/backend'

--- app/models/card.py
+++ app/models/card.py
@@ -7,20 +7,11 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer, nullable=True)
     board = db.relationship("Board", back_populates="cards", lazy=True)
     board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
-<<<<<<< HEAD
-    required_fields = ["message","likes_count","board_id"]
-=======
->>>>>>> d7390ee2c70f62448fd51cb5daad4f9a8c537e0a
 
     def to_dict(self):
         return {
             "card_id": self.id,
             "message": self.message,
-<<<<<<< HEAD
-            "likes_count": self.likes_count,
-            "board_id": self.board_id,
-=======
             "likes_count": self.like_count,
-            "board_id": self.board.id,
->>>>>>> d7390ee2c70f62448fd51cb5daad4f9a8c537e0a
+            "board_id": self.board_id,
         }
